0568c292c33b3feccdd2c95bf9e98ef0fc162ea4
Alchyr <isithalchyr@gmail.com>
2026-04-16 12:49:21 -0700

    properly respect config option and check for actual error rather than also triggering on warnings

--- Patches/Utils/LogPatch.cs
+++ Patches/Utils/LogPatch.cs
@@ -15,7 +15,7 @@ public partial class LogListener : Godot.Logger
     {
         NLogWindow.AddLog(message);
 
-        if (error)
+        if (error && message.Contains("ERROR"))
         {
             NLogWindow.OpenOnErr();
         }
